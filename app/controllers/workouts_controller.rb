b79b16e858806208f552d0b9a795da3be1299435
skarimo <karimovj@gmail.com>
2018-09-10 15:49:09 -0400

    added workout create and show page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,41 @@
 class WorkoutsController < ApplicationController
+
+  def index
+    @workouts = Workout.all
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
+  def new
+      @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+      if @workout.valid?
+        @workout.save
+        redirect_to @workout
+      else
+        render :new
+      end
+  end
+
+
+
+  private
+  def workout_params
+    params.require(:workout).permit(:name, :description, :instructions)
+  end
+
+
+
+
+
+
+
+
+
+
 end
